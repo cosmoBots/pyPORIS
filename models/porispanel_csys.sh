a5088b478423dfd863a53a443fc546b1ce10822a
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-05-07 08:04:02 +0100

    Adding escape points for cancelling the process in case of failre.

--- models/porispanel_csys.sh
+++ models/porispanel_csys.sh
@@ -2,8 +2,8 @@
 rm $1.ods
 rm $1.xml
 cp ../config_rm_enabled.py ../config_rm.py
-python3 ../graph2poris.py $1.graphml
+python3 ../graph2poris.py $1.graphml || { echo "graph2poris could not be processed"; exit 1; } 
 mv $1.graphml $1.old.graphml
 mv $1.out.graphml $1.graphml
-python3 ../poris2xml.py $1.ods
+python3 ../poris2xml.py $1.ods || { echo "poris2xml could not be processed"; exit 1; } 
 java -jar ../AstroPorisPlayer/bin/AstroPorisPlayer.jar $1.xml
