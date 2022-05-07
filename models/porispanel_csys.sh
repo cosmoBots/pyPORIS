5a59569c71202818e387e5525a5ff40315521203
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-05-07 07:23:24 +0100

    Creating two scripts for working with or without cosmosys support

--- models/porispanel_csys.sh
+++ models/porispanel_csys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 rm $1.ods
 rm $1.xml
-cp ../config_rm_disabled.py ../config_rm.py
+cp ../config_rm_enabled.py ../config_rm.py
 python3 ../graph2poris.py $1.graphml
+mv $1.graphml $1.old.graphml
+mv $1.out.graphml $1.graphml
 python3 ../poris2xml.py $1.ods
 java -jar ../AstroPorisPlayer/bin/AstroPorisPlayer.jar $1.xml
