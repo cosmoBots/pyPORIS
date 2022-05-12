a2f5921b3a5a9df8940adbfd8a3b84bc18a03095
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-05-12 18:45:01 +0100

    Preventing loosing the updated GraphML if the user saves an open model

--- porispanel_csys.sh
+++ porispanel_csys.sh
@@ -2,7 +2,9 @@
 rm models/$1.ods
 rm models/$1.xml
 cp config_rm_enabled.py config_rm.py
-python3 graph2poris.py models/$1.graphml || { echo "graph2poris could not be processed"; exit 1; } 
+python3 graph2poris.py models/$1.graphml || { echo "graph2poris could not be processed"; exit 1; }
+timestamp=$(date +%s)
+cp models/$1.graphml models/$1.$timestamp.backup
 mv models/$1.graphml models/$1.old.graphml
 mv models/$1.out.graphml models/$1.graphml
 python3 poris2xml.py models/$1.ods || { echo "poris2xml could not be processed"; exit 1; } 
