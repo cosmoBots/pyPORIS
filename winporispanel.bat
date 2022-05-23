8ffcb92259210428c79265e8299a20e5807d0ef4
Txinto Vaz <txinto.vaz@gtc.iac.es>
2022-05-23 10:52:49 +0200

    Updating Windows script

--- winporispanel.bat
+++ winporispanel.bat
@@ -5,13 +5,13 @@ if [%1]==[] (
 ) else (
     del models\%1.ods
     del models\%1.xml
-    copy .\pyPORIS\config_rm_disabled.py pyPORIS\config_rm.py
+    copy .\config_rm_disabled.py .\config_rm.py
 
-    python pyPORIS\graph2poris.py models\%1.graphml
+    python graph2poris.py models\%1.graphml
     if exist .\models\%1.ods (
-        python pyPORIS\poris2xml.py models\%1.ods
+        python poris2xml.py models\%1.ods
         if exist .\models\%1.xml (
-            java -jar pyPORIS\AstroPorisPlayer\bin\AstroPorisPlayer.jar models\%1.xml
+            java -jar AstroPorisPlayer\bin\AstroPorisPlayer.jar models\%1.xml
         ) else (
             echo "graph2poris could not be processed"
             echo A second statement
