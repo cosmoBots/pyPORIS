68dffa85c4f1df73c7bba79136ee14d955cc6ea5
Txinto Vaz <txinto@elporis.com>
2024-09-03 14:35:51 +0100

    Generating example python classes

---
+++ output/example/example_physical/example_physical.py
@@ -0,0 +1,16 @@
+from examplePORIS import *
+
+class example_physical(examplePORIS):
+    # Go to ARCGengIIIPORIS.py, navigate to the ##### Action triggers ##### section
+    # which is normally at the bottom of the class, and copy here the methods 
+    # to start overriding them, in order to convert the virtual device into a physical one
+    # Once this class has any content, remove the pass clause
+
+    pass
+
+
+thismodel = example_physical()
+
+print("Let's test our model ",thismodel.root.getName())
+print("Current mode is ",thismodel.root.getSelectedMode().getName())
+
